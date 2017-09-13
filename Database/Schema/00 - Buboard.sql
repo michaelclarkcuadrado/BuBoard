7393557836d4134b2367296867f687942a178234
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-09-13 17:08:36 -0400

    Add First Buboard Schema

--- Database/Schema/00 - Buboard.sql
+++ Database/Schema/00 - Buboard.sql
@@ -1,3 +1,25 @@
 GRANT ALL PRIVILEGES ON * . * TO 'buboard' IDENTIFIED BY 'buboard' WITH GRANT OPTION;
+CREATE DATABASE `buboard-data` CHARACTER SET UTF8 COLLATE utf8_bin;
 
-CREATE DATABASE `buboard-data`;
+USE `buboard-data`;
+/* Create tables */
+CREATE TABLE `buboard-data`.`post_categories` ( `category_id` INT NOT NULL AUTO_INCREMENT , `category_name` VARCHAR(255) NOT NULL , PRIMARY KEY (`category_id`));
+
+CREATE TABLE `buboard-data`.`buboard_posts` ( `post_id` INT NOT NULL AUTO_INCREMENT , `post_by_user_id` INT NOT NULL , `belongs_to_category` INT NOT NULL , `post_contents` VARCHAR(255) NOT NULL , `post_title` VARCHAR(255) NOT NULL , `post_date` DATETIME NOT NULL , `has_attached_photo` TINYINT(1) NOT NULL , PRIMARY KEY (`post_id`));
+
+CREATE TABLE `buboard-data`.`buboard_profiles` ( `profile_id` INT NOT NULL AUTO_INCREMENT , `real_name` VARCHAR(255) NOT NULL , `date_signup` DATETIME NOT NULL , `email_address` VARCHAR(320) NOT NULL , `email_is_confirmed` TINYINT(1) NOT NULL , `profile_desc` VARCHAR(255) NOT NULL , `has_submitted_photo` TINYINT(1) NOT NULL , PRIMARY KEY (`profile_id`));
+
+CREATE TABLE `buboard-data`.`profile_follows` ( `follower_id` INT NOT NULL , `following_id` INT NOT NULL, PRIMARY KEY( `follower_id`, `following_id`));
+
+/* Create foriegn keys between tables */
+
+# Posts 2 categories
+ALTER TABLE `buboard_posts` ADD CONSTRAINT `posts2categories` FOREIGN KEY (`belongs_to_category`) REFERENCES `post_categories`(`category_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+# Follows to users
+ALTER TABLE `profile_follows` ADD CONSTRAINT `followers2users` FOREIGN KEY (`follower_id`) REFERENCES `buboard_profiles`(`profile_id`) ON DELETE RESTRICT ON UPDATE RESTRICT; 
+ALTER TABLE `profile_follows` ADD CONSTRAINT `followed2users` FOREIGN KEY (`following_id`) REFERENCES `buboard_profiles`(`profile_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+
+
+# Posts 2 users
+ALTER TABLE `buboard_posts` ADD CONSTRAINT `posts2users` FOREIGN KEY (`post_by_user_id`) REFERENCES `buboard_profiles`(`profile_id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- README.md
+++ README.md
@@ -3,13 +3,16 @@ CS440 - BuBoard Assignment
 
 ## Starting in Docker
 
-### First Run
+### First Run:
 Clone the repo, then run `docker-compose up`
 
 ### To reflect changes in code after making them:
 `docker-compose up --build`
 
-### To reset the database to a blank slate from the schema
-`docker-compose down`
-`docker rm buboard_buboard-database_1 `
-`docker volume rm buboard_buboard-db`
+### To reset the database to a blank slate from the schema:
+```
+docker-compose down
+docker rm buboard_buboard-database_1 
+docker volume rm buboard_buboard-db
+docker-compose up --build
+```

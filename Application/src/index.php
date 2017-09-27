0496eda5ba8d9e9b622020f464b6c6af6621a45d
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-09-27 04:12:07 -0400

    Add mockup login screen, fix db schema

--- Application/src/index.php
+++ Application/src/index.php
@@ -1,321 +1,80 @@
-
-<html>
-<style>
-
-* { box-sizing:border-box; }
-
-body {
-	font-family: Helvetica;
-	background: #eee;
-  -webkit-font-smoothing: antialiased;
-}
-
-hgroup {
-	text-align:center;
-	margin-top: 4em;
-}
-
-h1, h3 { font-weight: 300; }
-
-h1 { color: #636363; }
-
-h3 { color: #4a89dc; }
-
-form {
-	width: 380px;
-	margin: 4em auto;
-	padding: 3em 2em 2em 2em;
-	background: #fafafa;
-	border: 1px solid #ebebeb;
-	box-shadow: rgba(0,0,0,0.14902) 0px 1px 1px 0px,rgba(0,0,0,0.09804) 0px 1px 2px 0px;
-}
-
-.group {
-	position: relative;
-	margin-bottom: 45px;
-}
-
-input {
-	font-size: 18px;
-	padding: 10px 10px 10px 5px;
-	-webkit-appearance: none;
-	display: block;
-	background: #fafafa;
-	color: #636363;
-	width: 100%;
-	border: none;
-	border-radius: 0;
-	border-bottom: 1px solid #757575;
-}
-
-input:focus { outline: none; }
-
-
-/* Label */
-
-label {
-	color: #999;
-	font-size: 18px;
-	font-weight: normal;
-	position: absolute;
-	pointer-events: none;
-	left: 5px;
-	top: 10px;
-	transition: all 0.2s ease;
-}
-
-
-/* active */
-
-input:focus ~ label, input.used ~ label {
-	top: -20px;
-  transform: scale(.75); left: -2px;
-	/* font-size: 14px; */
-	color: #4a89dc;
-}
-
-
-/* Underline */
-
-.bar {
-	position: relative;
-	display: block;
-	width: 100%;
-}
-
-.bar:before, .bar:after {
-	content: '';
-	height: 2px;
-	width: 0;
-	bottom: 1px;
-	position: absolute;
-	background: #4a89dc;
-	transition: all 0.2s ease;
-}
-
-.bar:before { left: 50%; }
-
-.bar:after { right: 50%; }
-
-
-/* active */
-
-input:focus ~ .bar:before, input:focus ~ .bar:after { width: 50%; }
-
-
-/* Highlight */
-
-.highlight {
-	position: absolute;
-	height: 60%;
-	width: 100px;
-	top: 25%;
-	left: 0;
-	pointer-events: none;
-	opacity: 0.5;
-}
-
-
-/* active */
-
-input:focus ~ .highlight {
-	animation: inputHighlighter 0.3s ease;
-}
-
-
-/* Animations */
-
-@keyframes inputHighlighter {
-	from { background: #4a89dc; }
-	to 	{ width: 0; background: transparent; }
-}
-
-
-/* Button */
-
-.button {
-  position: relative;
-  display: inline-block;
-  padding: 12px 24px;
-  margin: .3em 0 1em 0;
-  width: 100%;
-  vertical-align: middle;
-  color: #fff;
-  font-size: 16px;
-  line-height: 20px;
-  -webkit-font-smoothing: antialiased;
-  text-align: center;
-  letter-spacing: 1px;
-  background: transparent;
-  border: 0;
-  border-bottom: 2px solid #3160B6;
-  cursor: pointer;
-  transition: all 0.15s ease;
-}
-.button:focus { outline: 0; }
-
-
-/* Button modifiers */
-
-.buttonBlue {
-  background: #4a89dc;
-  text-shadow: 1px 1px 0 rgba(39, 110, 204, .5);
-}
-
-.buttonBlue:hover { background: #357bd8; }
-
-
-/* Ripples container */
-
-.ripples {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  background: transparent;
-}
-
-
-/* Ripples circle */
-
-.ripplesCircle {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0;
-  width: 0;
-  height: 0;
-  border-radius: 50%;
-  background: rgba(255, 255, 255, 0.25);
-}
-
-.ripples.is-active .ripplesCircle {
-  animation: ripples .4s ease-in;
-}
-
-
-/* Ripples animation */
-
-@keyframes ripples {
-  0% { opacity: 0; }
-
-  25% { opacity: 1; }
-
-  100% {
-    width: 200%;
-    padding-bottom: 200%;
-    opacity: 0;
-  }
-}
-
-footer { text-align: center; }
-
-footer p {
-	color: #888;
-	font-size: 13px;
-	letter-spacing: .4px;
-}
-
-footer a {
-	color: #4a89dc;
-	text-decoration: none;
-	transition: all .2s ease;
-}
-
-footer a:hover {
-	color: #666;
-	text-decoration: underline;
-}
-
-footer img {
-	width: 80px;
-	transition: all .2s ease;
-}
-
-footer img:hover { opacity: .83; }
-
-footer img:focus , footer a:focus { outline: none; }
-
-</style>
-
-<?php
-require_once 'config.php';
-echo "Hello World! <br> <br>";
-
-echo "Data will be below if the database is working: <br> <br>";
-
-$data = mysqli_query($mysqli, "DESCRIBE buboard_profiles");
-while($row = mysqli_fetch_assoc($data)){
-    print_r($row);
-    echo "<br>";
-}
-?>
-
-<script>
-
-
-$(window, document, undefined).ready(function() {
-
-  $('input').blur(function() {
-    var $this = $(this);
-    if ($this.val())
-      $this.addClass('used');
-    else
-      $this.removeClass('used');
-  });
-
-  var $ripples = $('.ripples');
-
-  $ripples.on('click.Ripples', function(e) {
-
-    var $this = $(this);
-    var $offset = $this.parent().offset();
-    var $circle = $this.find('.ripplesCircle');
-
-    var x = e.pageX - $offset.left;
-    var y = e.pageY - $offset.top;
-
-    $circle.css({
-      top: y + 'px',
-      left: x + 'px'
-    });
-
-    $this.addClass('is-active');
-
-  });
-
-  $ripples.on('animationend webkitAnimationEnd mozAnimationEnd oanimationend MSAnimationEnd', function(e) {
-  	$(this).removeClass('is-active');
-  });
-
-});
-
-</script>
-
-<
-
-<hgroup>
-  <h2>BuBoard Image Here</h2>
-  <h1>BuBoard</h1>
-</hgroup>
-<form>
-  <div class="group">
-    <input type="email"><span class="highlight"></span><span class="bar"></span>
-    <label>Email</label>
-  </div>
-  <div class="group">
-    <input type="text"><span class="highlight"></span><span class="bar"></span>
-    <label>Password</label>
-  </div>
-  <button type="button" class="button buttonBlue">Log In
-    <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
-  </button>
-
-  <button type="button" class="button buttonBlue">Creat New Account
-      <div class="ripples buttonRipples"><span class="ripplesCircle"></span></div>
-  </button>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="static/css/material.min.css">
+    <script defer src="static/js/material.min.js"></script>
+
+</head>
+<body>
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <!-- Title -->
+            <!-- Add spacer, to align navigation to the right -->
+            <!-- Navigation. We hide it in small screens. -->
+            <nav class="mdl-navigation mdl-layout--large-screen-only">
+            </nav>
+        </div>
+    </header>
+    <style>
+        .card-me {
+            padding-left: 25px;
+            padding-right: 25px;
+            padding-bottom: 25px;
+        }
+        .card-me > .mdl-card__title {
+            color: #fff;
+            height: 176px;
+            background: url('static/image/buboard_logo.jpg') center / cover;
+        }
+    </style>
+    <main class="mdl-layout__content">
+        <div class="page-content">
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--4-col"></div>
+                <div class="mdl-cell mdl-cell--4-col"></div>
+                <div class="mdl-cell mdl-cell--4-col"></div>
+            </div>
+            <div class="mdl-grid">
+                <div class="mdl-layout-spacer"></div>
+                <div class="wrapper">
+
+                    <div class="demo-card-wide mdl-card mdl-shadow--8dp card-me">
+                        <h4 align="center">Login</h4>
+
+                        <div align="center">
+                            <form action="#">
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="text" id="email">
+                                    <label class="mdl-textfield__label" for="email">Email or Phone</label>
+                                </div>
+
+                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                                    <input class="mdl-textfield__input" type="password" id="password">
+                                    <label class="mdl-textfield__label" for="password">Password</label>
+                                </div>
+
+                                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                    Login
+                                </button>
+                            </form>
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="mdl-layout-spacer"></div>
+            </div>
+            <div class="mdl-grid">
+                <div class="mdl-cell mdl-cell--4-col"></div>
+                <div class="mdl-cell mdl-cell--4-col"></div>
+                <div class="mdl-cell mdl-cell--4-col"></div>
+            </div>
+        </div>
+    </main>
+</div>
+</body>
 </html>
--- Database/Schema/00 - Buboard.sql
+++ Database/Schema/00 - Buboard.sql
@@ -7,7 +7,7 @@ CREATE TABLE `buboard-data`.`post_categories` ( `category_id` INT NOT NULL AUTO_
 
 CREATE TABLE `buboard-data`.`buboard_posts` ( `post_id` INT NOT NULL AUTO_INCREMENT , `post_by_user_id` INT NOT NULL , `belongs_to_category` INT NOT NULL , `post_contents` VARCHAR(255) NOT NULL , `post_title` VARCHAR(255) NOT NULL , `post_date` DATETIME NOT NULL , `has_attached_photo` TINYINT(1) NOT NULL , PRIMARY KEY (`post_id`));
 
-CREATE TABLE `buboard-data`.`buboard_profiles` ( `profile_id` INT NOT NULL AUTO_INCREMENT , `real_name` VARCHAR(255) NOT NULL , `date_signup` DATETIME NOT NULL , `email_address` VARCHAR(320) NOT NULL , `email_is_confirmed` TINYINT(1) NOT NULL , `profile_desc` VARCHAR(255) NOT NULL , `has_submitted_photo` TINYINT(1) NOT NULL , PRIMARY KEY (`profile_id`));
+CREATE TABLE `buboard-data`.`buboard_profiles` ( `profile_id` INT NOT NULL AUTO_INCREMENT , `real_name` VARCHAR(255) NOT NULL , `date_signup` DATETIME NOT NULL , `password_hash` VARCHAR(255) NOT NULL, `email_confirmation_secret` VARCHAR(255) NOT NULL, `email_address` VARCHAR(320) NOT NULL , `email_is_confirmed` TINYINT(1) NOT NULL , `profile_desc` VARCHAR(255) NOT NULL , `has_submitted_photo` TINYINT(1) NOT NULL , PRIMARY KEY (`profile_id`));
 
 CREATE TABLE `buboard-data`.`profile_follows` ( `follower_id` INT NOT NULL , `following_id` INT NOT NULL, PRIMARY KEY( `follower_id`, `following_id`));
 

1f0b144de87477bbf92b0e15aba9a4ceaf05e90e
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-10-02 12:50:18 -0400

    Cleanup, automatic docker src updates, etc

---
+++ Application/src/feed.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html>
+<head>
+    <title>BuBoard Feed</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="static/css/material.min.css"/>
+    <link rel="stylesheet" href="static/css/feed.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="mdl-color--light-blue-A100">
+<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+            <span class="mdl-layout-title">Local Feed</span>
+            <div class="mdl-layout-spacer"></div>
+            <a class="mdl-navigation__link" href="help.php">
+                <i class="material-icons">help</i>
+            </a>
+        </div>
+    </header>
+    <div class="mdl-layout__drawer">
+        <span class="mdl-layout-title">BuBoard</span>
+        <nav class="mdl-navigation mdl-color--blue-light_blue-800">
+            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i> Home</a>
+            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">forum</i> Personal Feed</a>
+            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">flag</i> My Profile</a>
+            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">people</i>Tag 3</a>
+        </nav>
+    </div>
+
+    <!-- Creates the main content of the page -->
+    <main class="mdl--layout__content ">
+        <div class="mdl-grid mdl-color--grey-300">
+            <div class="mdl-cell mdl-cell--1-col mdl-cell--middle">
+
+
+            </div>
+        </div>
+    </main>
+    <div class="mdl-layout-spacer"></div>
+
+</div>
+
+</body>
+<script src="static/js/material.min.js"></script>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Application/src/index.php
+++ Application/src/index.php
@@ -1,80 +1,90 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    <link rel="stylesheet" href="static/css/material.min.css">
-    <script defer src="static/js/material.min.js"></script>
+<html>
+<!-- test -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue-light_blue.min.css" />
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 
-</head>
+<style>
+.mdl-card__title > img {
+  max-width: 100%;
+}
+
+
+.mdl-layout {
+  align-items: center;
+  justify-content: center;
+}
+
+body{
+	background-color: #80d8ff;
+}
+</style>
+
 <body>
-<div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-        <div class="mdl-layout__header-row">
-            <!-- Title -->
-            <!-- Add spacer, to align navigation to the right -->
-            <!-- Navigation. We hide it in small screens. -->
-            <nav class="mdl-navigation mdl-layout--large-screen-only">
-            </nav>
-        </div>
-    </header>
-    <style>
-        .card-me {
-            padding-left: 25px;
-            padding-right: 25px;
-            padding-bottom: 25px;
-        }
-        .card-me > .mdl-card__title {
-            color: #fff;
-            height: 176px;
-            background: url('static/image/buboard_logo.jpg') center / cover;
-        }
-    </style>
-    <main class="mdl-layout__content">
-        <div class="page-content">
-            <div class="mdl-grid">
-                <div class="mdl-cell mdl-cell--4-col"></div>
-                <div class="mdl-cell mdl-cell--4-col"></div>
-                <div class="mdl-cell mdl-cell--4-col"></div>
-            </div>
-            <div class="mdl-grid">
-                <div class="mdl-layout-spacer"></div>
-                <div class="wrapper">
 
-                    <div class="demo-card-wide mdl-card mdl-shadow--8dp card-me">
-                        <h4 align="center">Login</h4>
+<!--
+This div here should center this log in screen on any page that the user is on
+-->
+<main class="mdl--layout__content">
 
-                        <div align="center">
-                            <form action="#">
-                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="text" id="email">
-                                    <label class="mdl-textfield__label" for="email">Email or Phone</label>
-                                </div>
+	<!-- Subsection of the main page -->
+	<div class="page-content"> 
+			<!-- Centers the card on the page -->
+			<div class="mdl-layout">
+			<div class="mdl-layout mdl-js-layout" style="width:800px; margin:0 auto;">
 
-                                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
-                                    <input class="mdl-textfield__input" type="password" id="password">
-                                    <label class="mdl-textfield__label" for="password">Password</label>
-                                </div>
 
-                                <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-                                    Login
-                                </button>
-                            </form>
-                        </div>
+				<div id="cube" class="show-front">
+				    <figure class="front">
+					<div class="mdl-card mdl-shadow--6dp">
+					    <div class="mdl-card__title mdl-color--primary mdl-color-text--white relative">
+						<img src = "static/image/buboard_logo.jpg"/>
+						<h2 class="mdl-card__title-text">Buboard Login</h2>
+					    </div>
 
-                    </div>
+					    <div class="mdl-card__supporting-text">
+						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+						    <input class="mdl-textfield__input" id="login"/>
+						    <label class="mdl-textfield__label" for="login">Email or Username</label>
 
-                </div>
-                <div class="mdl-layout-spacer"></div>
-            </div>
-            <div class="mdl-grid">
-                <div class="mdl-cell mdl-cell--4-col"></div>
-                <div class="mdl-cell mdl-cell--4-col"></div>
-                <div class="mdl-cell mdl-cell--4-col"></div>
-            </div>
-        </div>
-    </main>
-</div>
+						</div>
+						<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+						    <input class="mdl-textfield__input" type="password" id="password"/>
+						    <label class="mdl-textfield__label" for="password">Password</label>
+						</div>
+					    </div>
+
+					    <div class="mdl-card__actions mdl-card--border">
+						<div class="mdl-grid">
+						    <button class="mdl-cell mdl-cell--12-col mdl-button mdl-button--raised mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white">
+						        Sign In
+						    </button>
+
+						    <button class="mdl-cell mdl-cell--12-col mdl-button mdl-button--raised mdl-button--colored mdl-js-button mdl-js-ripple-effect mdl-color-text--white">
+						        Sign Up
+						    </button>
+				
+
+
+
+						</div>
+
+						<div class="mdl-grid">
+						    <div class="mdl-cell mdl-cell--12-col">
+						        <a onClick='index.php' class="mdl-color-text--primary" style="float: right">Forgot Password</a>
+						    </div>
+						</div>
+					    </div>
+					</div>
+				    </figure>
+
+				</div>
+			</div>
+	</div>
+	</div>
+</main>
+
+
 </body>
+
 </html>

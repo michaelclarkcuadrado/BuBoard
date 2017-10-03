33665fb71b315c5a4783a7c0117591e3a689d989
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2017-10-03 13:08:51 -0400

    Cards: Template done-ish

--- Application/src/feed.php
+++ Application/src/feed.php
@@ -25,16 +25,43 @@
             <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">home</i> Home</a>
             <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">forum</i> Personal Feed</a>
             <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">flag</i> My Profile</a>
-            <a class="mdl-navigation__link" href=""><i class="mdl-color-text--blue-grey-400 material-icons" role="presentation">people</i>Tag 3</a>
         </nav>
     </div>
 
     <!-- Creates the main content of the page -->
     <main class="mdl--layout__content ">
-        <div class="mdl-grid mdl-color--grey-300">
-            <div class="mdl-cell mdl-cell--1-col mdl-cell--middle">
-
-
+        <div class="mdl-grid">
+            <div class="mdl-card mdl-shadow--4dp mdl-cell mdl-cell--4-col">
+                <div class="mdl-card__title mdl-color--blue">
+                    <h2 class="mdl-card__title-text">
+                        Welcome to BuBoard!
+                    </h2>
+                </div>
+                <div class="mdl-card__supporting-text">
+                    This is a long text post about things and stuff. Many things are said here, regarding several other things. Sometimes, this text has substance. Sometimes, like this one, they do
+                    not.
+                    <div class="card-image-attachments">
+                        <hr>
+                        Attachments:
+                        <div>
+                            <a target="_blank" href="static/image/buboard_logo.jpg"><img class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone" src="static/image/buboard_logo.jpg"></a>
+                            <a target="_blank" href="static/image/buboard_logo.jpg"><img class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone" src="static/image/buboard_logo.jpg"></a>
+                            <a target="_blank" href="static/image/buboard_logo.jpg"><img class="mdl-cell mdl-cell--2-col mdl-cell--1-col-phone" src="static/image/buboard_logo.jpg"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="mdl-card__menu">
+                    <button id="demo-menu-lower-right"
+                            class="mdl-button mdl-js-button mdl-button--icon">
+                        <i class="material-icons">more_vert</i>
+                    </button>
+                    <ul class="mdl-menu mdl-menu--bottom-right mdl-js-menu mdl-js-ripple-effect"
+                        for="demo-menu-lower-right">
+                        <li class="mdl-menu__item">Follow User</li>
+                        <li class="mdl-menu__item">RSVP</li>
+                        <li class="mdl-menu__item">Delete Post</li>
+                    </ul>
+                </div>
             </div>
         </div>
     </main>

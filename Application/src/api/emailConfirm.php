e8a8d1b39c28bded0eb8c3beb9aa19c1d2c7e47a
Unknown <millza04@gettysburg.edu>
2017-10-25 14:10:22 -0400

    Front end changes to email confirmation

--- Application/src/api/emailConfirm.php
+++ Application/src/api/emailConfirm.php
@@ -1,8 +1,8 @@
 <?php
 require '../config.php';
 
-$email = mysqli_real_escape_string($_GET['email']);
-$confirmcode = $_GET['confirmcode'];
+//$email = mysqli_real_escape_string($_GET['email']);
+//$confirmcode = $_GET['confirmcode'];
 
 $result = mysqli_query($mysqli, "
 SELECT *
@@ -22,12 +22,111 @@ if ($confirmcode === $db_code){
     WHERE email_address='$email';
     ");
 
-    echo "Email Confirmed: Feel free to log on to BuBoard";
+    $confirmed = "Email Confirmed: Feel free to log on to BuBoard";
+
 
 
 }
 else{
-    echo "Invalid email code";
+    $confirmed = "Invalid email code";
 }
 
 ?>
+
+<html>
+<head>
+    <title>Welcome To BuBoard</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="../static/css/material.min.css"/>
+    <link rel="stylesheet" type="text/css" href="../static/css/signup.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<style>
+
+    .mdl-grid.center-items {
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        justify-content: center;
+    }
+
+    .main{
+        padding-top: 20px;
+    }
+
+    .mainbody{
+        text-align:center;
+        background-color: #80d8ff;
+        height: 50%;
+        width: 40%;
+        align-items: center;
+        padding-top: 20px;
+        padding-bottom: 20px;
+    }
+
+    .title{
+        text-align:center;
+    }
+    body {
+        background-image: url('../static/image/corktile.jpg');
+        background-attachment: local;
+        padding-top: 20px;
+    }
+
+    .text{
+        border-color: gray;
+        border-style: solid;
+        border-radius: 20px;
+    }
+
+    .wide{
+        border-color: gray;
+        border-style: solid;
+        border-radius: 20px;
+    }
+
+
+
+    @media (max-width: 700px) {
+        .mdl-grid.center-items {
+            background-color: white;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+        }
+
+        .mainbody{
+            background-color: #80d8ff;
+            height: 90%;
+            width: 92%;
+            align-items: center;
+            padding-top: 15px;
+            padding-bottom: 15px;
+        }
+    }
+
+
+</style>
+
+<main class="mdl--layout main">
+    <form name="mainform"  method="post" onsubmit="return validateForm()" action="api/newProfileSubmit.php" enctype="multipart/form-data">
+        <div class="mdl-grid mainbody mdl-shadow--8dp">
+            <div class="mdl-grid center-items mdl-shadow--8dp mdl-color--grey-100">
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone">
+                    <img class="thumbtack" src="../static/image/thumbtack.png">
+                </div>
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone"></div>
+                <h2 class="mdl-card__title-text mdl-color-text--blue-grey-600 title"><?php echo $confirmed; ?></h2>
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone"></div>
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone"></div>
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone"></div>
+                <div class="mdl-cell--12-col-desktop mdl-cell--5-col-phone"></div>
+            </div>
+        </div>
+    </form>
+</main>
+
+
+</html>
+

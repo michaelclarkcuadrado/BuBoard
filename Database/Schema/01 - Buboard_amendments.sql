21f1c77e43933ff327c3ebf0847722cc835c9edf
Michael Clark-Cuadrado <michael@michaelclarkcuadrado.com>
2018-02-11 20:12:05 -0500

    Make email logins case-insensitive

---
+++ Database/Schema/01 - Buboard_amendments.sql
@@ -0,0 +1,8 @@
+USE `buboard-data`;
+
+/* BEGIN CHANGES Feb 11th*/
+
+ALTER TABLE `buboard_profiles` CHANGE `email_address` `email_address` VARCHAR(320) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+
+
+/* END CHANGES Feb 11th*/
